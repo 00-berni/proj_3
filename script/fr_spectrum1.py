7ba7b11e14d171d763193a0e483ccf4b5029f92e
00-berni <bernardo.vettori@outlook.it>
2023-09-23 22:33:23 +0200

    Remove useless

--- script/fr_spectrum1.py
+++
@@ -1,8 +0,0 @@
-import os
-import numpy as np
-import matplotlib.pyplot as plt
-from scipy.signal import correlate
-from functions import *
-
-if __name__ == '__main__':
-    hdul, sp_data, spectrum, lengths, flat_value, cal_func = calibrated_spectrum(0,'betaLyr','giove')
