dca9b2644425a9433c4972eb65f16cf73cadbd0f
00-berni <bernardo.vettori@outlook.it>
2023-10-14 19:08:57 +0200

    Balmer series added

--- script.py
+++ script.py
@@ -1,7 +1,8 @@
 import numpy as np
+import matplotlib.pyplot as plt
 # from .spectralpy import data
 from spectralpy.calcorr import calibrated_spectrum, lamp_corr
-            
+from spectralpy.display import fastplot            
 
 if __name__ == '__main__':
 
@@ -12,7 +13,20 @@ if __name__ == '__main__':
 
     ### ANALYSIS
     spectrum, lengths, cal_res =  calibrated_spectrum(night,obj_name1,display_plots=display_plots, ret_values='calibration')
-    
+
+    fastplot(lengths, spectrum,title='Corrected and calibrated spectrum of ' + obj_name1,labels=['$\lambda$ [$\AA$]','counts'],dim=[17,9],grid=True)
+    plt.axvline(6563,0,1,color='r',alpha=0.5,label='H$_\\alpha$')
+    plt.axvline(4861,0,1,color='r',alpha=0.5,label='H$_\\beta$')
+    plt.axvline(4340,0,1,color='r',alpha=0.5,label='H$_\\gamma$')
+    plt.axvline(4102,0,1,color='r',alpha=0.5,label='H$_\\delta$')
+    plt.axvline(3970,0,1,color='r',alpha=0.5,label='H$_\\varepsilon$')
+    plt.axvline(3889,0,1,color='r',alpha=0.5,label='H$_\\xi$')
+    plt.axvline(3646,0,1,color='r',linestyle='--',alpha=0.5,label='Balmer Jump')
+    plt.legend(numpoints=1)
+    plt.show()
+
+
+
     angle1 = cal_res['angle']
     flat_value = cal_res['flat']
     cal_func = cal_res['func']
@@ -26,9 +40,32 @@ if __name__ == '__main__':
     obj_name2 = 'vega'
     spectrum, lengths, angle2 = calibrated_spectrum(night,obj_name2,flat=flat_value,cal_func=cal_func,err_func=err_func,display_plots=display_plots,ret_values='calibration')
 
+    fastplot(lengths, spectrum,title='Corrected and calibrated spectrum of ' + obj_name2,labels=['$\lambda$ [$\AA$]','counts'],dim=[17,9],grid=True)
+    plt.axvline(6563,0,1,color='r',alpha=0.5,label='H$_\\alpha$')
+    plt.axvline(4861,0,1,color='r',alpha=0.5,label='H$_\\beta$')
+    plt.axvline(4340,0,1,color='r',alpha=0.5,label='H$_\\gamma$')
+    plt.axvline(4102,0,1,color='r',alpha=0.5,label='H$_\\delta$')
+    plt.axvline(3970,0,1,color='r',alpha=0.5,label='H$_\\varepsilon$')
+    plt.axvline(3889,0,1,color='r',alpha=0.5,label='H$_\\xi$')
+    plt.axvline(3646,0,1,color='r',linestyle='--',alpha=0.5,label='Balmer Jump')
+    plt.legend(numpoints=1)
+    plt.show()
+
+
     lag = lamp_corr(night,(obj_name1,obj_name2),(angle1,angle2),display_plots=True)
 
     obj_name2 = 'gammaCygni'
     spectrum, lengths, angle2 = calibrated_spectrum(night,obj_name2,flat=flat_value,cal_func=cal_func,err_func=err_func,display_plots=display_plots,ret_values='calibration')
 
+    fastplot(lengths, spectrum,title='Corrected and calibrated spectrum of ' + obj_name2,labels=['$\lambda$ [$\AA$]','counts'],dim=[17,9],grid=True)
+    plt.axvline(6563,0,1,color='r',alpha=0.5,label='H$_\\alpha$')
+    plt.axvline(4861,0,1,color='r',alpha=0.5,label='H$_\\beta$')
+    plt.axvline(4340,0,1,color='r',alpha=0.5,label='H$_\\gamma$')
+    plt.axvline(4102,0,1,color='r',alpha=0.5,label='H$_\\delta$')
+    plt.axvline(3970,0,1,color='r',alpha=0.5,label='H$_\\varepsilon$')
+    plt.axvline(3889,0,1,color='r',alpha=0.5,label='H$_\\xi$')
+    plt.axvline(3646,0,1,color='r',linestyle='--',alpha=0.5,label='Balmer Jump')
+    plt.legend(numpoints=1)
+    plt.show()
+ 
     lag = lamp_corr(night,(obj_name1,obj_name2),(angle1,angle2),display_plots=True)
