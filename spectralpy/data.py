0321fb6871788adc74e23b5ea717f5170253e161
00-berni <bernardo.vettori@outlook.it>
2024-08-11 23:42:54 +0200

    start updating docstrings

--- spectralpy/data.py
+++ spectralpy/data.py
@@ -42,36 +42,27 @@ def data_extraction(path_file: str) -> dict:
     Returns
     -------
     data_file : dict
-        data organized into nights of aquisition and objects
+        data organized into nights of aquisition and targets names
     """
     import json 
-    # opening the file
+    # open the file
     with open(path_file) as f:
         data_file = f.read()
-    # converting in a python dictonary
+    # convert in a python dictonary
     data_file = json.loads(data_file)
     return data_file
 
-# names for the selection of the night observation
-NIGHTS = ('16-08-26','17-03-27','18-04-22','18-11-27','22-07-26_ohp','22-07-26_ohp','23-03-28')
-# taking path of the current folder
-PWD = os.path.dirname(os.path.realpath(__file__))
-# path of the project folder
-PROJECT_DIR = os.path.split(PWD)[0]
-# path of the data folder
-DATA_DIR = os.path.join(PROJECT_DIR, 'data_files')
-# path of calibration folder
-CAL_DIR = os.path.join(DATA_DIR, 'calibration')
-# path of results folder
-RESULT_DIR = os.path.join(PROJECT_DIR, 'results')
 
-# file with objects collection
-OBJ_FILE = os.path.join(DATA_DIR, 'objs_per_night.json')
+NIGHTS = ('16-08-26','17-03-27','18-04-22','18-11-27',
+          '22-07-26_ohp','22-07-26_ohp','23-03-28')         #: nights of observation
+PWD = os.path.dirname(os.path.realpath(__file__))           #: path of the current dir
+PROJECT_DIR = os.path.split(PWD)[0]                         #: path of the project dir
+DATA_DIR = os.path.join(PROJECT_DIR, 'data_files')          #: path of the data dir
+CAL_DIR = os.path.join(DATA_DIR, 'calibration')             #: path of calibration dir
+RESULT_DIR = os.path.join(PROJECT_DIR, 'results')           #: path of results dir
+OBJ_FILE = os.path.join(DATA_DIR, 'objs_per_night.json')    #: path of file with objects collection
+DATA_ALL = data_extraction(OBJ_FILE)                        #: dictionary with the targets per night
 
-# extracting data
-DATA_ALL = data_extraction(OBJ_FILE)
-
-# Literal['16-08-26','17-03-27','18-04-22','18-11-27','22-07-26_ohp','22-07-26_ohp','23-03-28']
 def collect_fits(night: str, obj: str) -> tuple[NDArray, ArrayLike]:
     """Collecting data fits for a chosen night observation and object.
 
@@ -80,7 +71,7 @@ def collect_fits(night: str, obj: str) -> tuple[NDArray, ArrayLike]:
     night : str
         selected night
     obj : str
-        name of the object
+        name of the target
 
     Returns
     -------
@@ -89,7 +80,11 @@ def collect_fits(night: str, obj: str) -> tuple[NDArray, ArrayLike]:
     cut : ArrayLike
         section limits for the images
     """
+    # take target name
     extracted = DATA_ALL[night][obj]
+    # load the edges of the cut
+    #.. if the file `cut_indicies.txt` is absent
+    #.. then one is generated
     lims_path = os.path.join(DATA_DIR, night, obj, 'cut_indicies.txt')
     try:
         cut = np.loadtxt(lims_path, dtype=int, unpack=False)
@@ -99,79 +94,69 @@ def collect_fits(night: str, obj: str) -> tuple[NDArray, ArrayLike]:
     return extracted, cut
 
 def data_file_path(night: str, obj: str, data_file: str) -> str:
-    """
+    """ Compute the exact path of a fits file
 
     Parameters
     ----------
     night : int
-        _description_
+        selected observation night
     obj : str
-        _description_
+        name of the target
     data_file : str
-        _description_
+        selected fits file fot that target
 
     Returns
     -------
     str
-        _description_
+        the path of the file
     """
     return os.path.join(DATA_DIR, night, obj, data_file + '.fit')
 
 ##* 
 def get_data_fit(path: str, lims: Sequence[int | None] = [0,None,0,None], hotpx: bool = True, display_plots: bool = True, **kwargs) -> Spectrum:
     """Function to open fits file and extract data.
-    It brings the path and extracts the data, giving a row image.
-    You can set a portion of image and also the correction for hotpx.
 
     Parameters
     ----------
     path : str
         path of the fits file
     lims : list, optional
-        edges of the fits, by default `[0,None,0,None]`
+        edges of the cut, by default `[0,None,0,None]`
         `lims` parameter controls the x and y extremes 
         in such the form `[lower y, higher y, lower x, higher x]`
     hotpx : bool, optional
-        parameter to remove or not the hot pixels, by default `True`
-        It calls the functions `hotpx_remove`
+        parameter to check and remove hot pixels, by default `True`
     display_plots : bool, optional
-        _description_, by default `True`
+        parameter to plot data, by default `True`
     **kwargs:
-        Parameters for the image
-
-        Properties:
-        * v : int
-            cmap parameter, by default `-1` 
-              -  `1` : false colors
-              -  `0` : grayscale
-              - `-1` : reversed grayscale
-        * title : str
-            title of the image, by default `''`
-        * n : int
-            figure number, by default `None`
-        * dim : list[int]
-            figure size, by default `[10,7]`
+        Parameters for the plot, see `display.showfits()`
 
     Returns
     -------
-    hdul : HDUList
-        information about the chosen fits file
-    data : NDArray
-        data of the spectrum
+    target : Spectrum
+        information about the selected spectrum, see `stuff.Spectrum`
+        
+    Notes
+    -----
+    Function brings the path and extracts the data, giving a row image.
+    You can set a portion of image and also the correction for hotpx.
+    
     """
     # open the file
     hdul = fits.open(path)
     # print fits info
     hdul.info()
-
     # data extraction
-    # format -> data[Y,X]
+    #.. format -> data[Y,X]
     data = hdul[0].data
     ly,ry,lx,rx = lims
+    # cut the image
     data = data[ly:ry,lx:rx]
+    # store in `Spectrum` class
     target = Spectrum(hdul, data, lims=lims, hotpx=hotpx)
+    # print the header
     target.print_header()
-    # Spectrum image
+    # display target image
     if display_plots == True: 
         from .display import showfits
         kwargs['subtitle'] = kwargs['subtitle'] + f', exp. time: {target.get_exposure()} s' 
@@ -179,57 +164,84 @@ def get_data_fit(path: str, lims: Sequence[int | None] = [0,None,0,None], hotpx:
     return target
 ##*
 
-def get_data(ch_obj: str, obj_fit: str, lims_fit: list[int | None] = [None,None,None,None] , angle: float | None = None, display_plots: bool = False,**kwargs) -> tuple[Spectrum, float]:
+def get_data(ch_obj: str, obj_fit: str, lims_fit: list[int | None] = [None,None,None,None] , angle: float | None = None, display_plots: bool = False) -> tuple[Spectrum, float]:
     """Extracting the fits data
-
+  
     Parameters
     ----------
     ch_obj : str
         chosen object name
     obj_fit : str
         path of the target
-    lims_fit : list[int  |  None], optional
+    lims_fit : list[int | None], optional
         extrema of the image, by default `[None,None,None,None]`
     angle : float | None, optional
         the inclination angle to rotate the image, by default `None`
-        If it is `None` it will be estimated
+        If it is `None` it will be estimated, see `stuff.angle_correction()`
     display_plots : bool, optional
-        if it is `True` images/plots are displayed, by default `False`
-
+        parameter to plot data, by default `True`
+   
     Returns
     -------
-    hdul : HDList
-        fits information
-    sp_data : NDArray
-        spectrum data
-    angle : float
-        inclination angle
+    target : Spectrum
+        information about the selected spectrum, see `stuff.Spectrum`
+        If `angle != 0` then the inclination of data is corrected by 
+        rotating rigidly
     
     Notes
     -----
     The function gets the data of the spectrum from the fits file of a selected target and corrects for the
-    inclination, returning the fits information (`hdul`), the spectrum data (`sp_data`) and the angle of
-    inclination (`angle`).
+    inclination.
     """
-
-    # collecting fits informations and spectrum data
+    # collect fits informations and spectrum data
     target = get_data_fit(obj_fit, lims=lims_fit, display_plots=display_plots, title='Row spectrum', subtitle=ch_obj, n=1)
-    # correcting for inclination angle
+    # correct for inclination angle
     angle, target.data = angle_correction(target.data, angle=angle, display_plots=display_plots)
-    # condition to display the images/plots
+    # display the target image
     if display_plots == True:
         showfits(target.data, title='Rotated image', subtitle=ch_obj+f', exp. time: {target.get_exposure()} s', n=2)
         plt.show()
     return target, angle
 
-def extract_cal_data(ch_obs: Literal['17-03-27','18-11-27','22-07-26_ohp','22-07-27_ohp','23-03-28'], sel_cal: Literal['dark','flat','bias','all'] = 'all', angle: float | None = 0) -> list[NDArray | list[NDArray]]:
+def extract_cal_data(ch_obs: Literal['17-03-27','18-11-27','22-07-26_ohp','22-07-27_ohp','23-03-28'], sel_cal: Literal['dark','flat','bias','all'] = 'all', angle: float | None = 0) -> list[Spectrum]:
+    """To get data of dark, flat or bias, if any
+
+    Parameters
+    ----------
+    ch_obs : str
+        selected night
+    sel_cal : Literal['dark','flat','bias','all'], optional
+        selected kind of calibration file, by default 'all'
+        If `sel_cal == 'all'` function returns all possible files
+    angle : float | None, optional
+        inclination angle of the slit, by default 0
+
+    Returns
+    -------
+    results : list[Spectrum]
+        function returns different results depending on the 
+        parameter `sel_cal` and the selected night of 
+        observation. If there are different acquisitions of 
+        dark, flat or bias, the mean is computed
+
+    Raises
+    ------
+    Exception
+        _description_
+    Exception
+        _description_
+    """
+    # initialize some quantities
     results = []
     dark = None
     flat = None
     bias = None
+    # only certain nights have information about calibration
     if ch_obs == '17-03-27':
+        # collect information
         calibration, lims = collect_fits(ch_obs,'calibrazione')
         if sel_cal in ['dark', 'all']:
+            # average on different acquisitions
             mean_dark = Spectrum.empty()
             dark = calibration['dark']
             for d in dark:
@@ -238,43 +250,68 @@ def extract_cal_data(ch_obs: Literal['17-03-27','18-11-27','22-07-26_ohp','22-07
                 mean_dark.hdul += [tmp.hdul]
                 mean_dark.data += [tmp.data]
             mean_dark.data = np.mean(mean_dark.data,axis=0)
+            # store the result
             results += [mean_dark]
         if sel_cal in ['flat', 'all']:
+            # average on different acquisitions
             mean_flat = Spectrum.empty()
             flat = calibration['flat'] 
-            for f in flat:
+            for (f, lim) in zip(flat,lims):
                 f = data_file_path(ch_obs,'calibrazione',f)
-                tmp, angle = get_data('flat', f, lims_fit=lims, angle=angle) 
+                tmp, angle = get_data('flat', f, lims_fit=lim, angle=angle) 
                 mean_flat.hdul += [tmp.hdul]
                 mean_flat.data += [tmp.data]
-                mean_flat.lims = lims
+                mean_flat.lims = lim
             mean_flat.data = np.mean(mean_flat.data,axis=0)
+            # store the result
             results += [mean_flat]
         if sel_cal in ['bias', 'all']:
+            # average on different acquisitions
             master_bias = Spectrum.empty() 
             bias = calibration['bias'] 
-            for d in dark:
-                d = data_file_path(ch_obs,'calibrazione',d)
-                tmp, angle = get_data('dark', d, angle=angle) 
+            for b in bias:
+                b = data_file_path(ch_obs,'calibrazione',b)
+                tmp, angle = get_data('bias', b, angle=angle) 
                 master_bias.hdul += [tmp.hdul]
                 master_bias.data += [tmp.data]
             master_bias.data = np.mean(master_bias.data,axis=0)
+            # store the result
             results += [master_bias]
     elif ch_obs in ['18-11-27','22-07-26_ohp','22-07-27_ohp']:
         calibration, lims = collect_fits(ch_obs,'Calibration')
         if sel_cal in ['flat', 'all']:
-            flat = calibration[0] 
-            results += [[flat, lims]]
+            flat = data_file_path(ch_obs,'Calibration',calibration)
+            flat, angle = get_data('flat', flat, lims, angle=angle) 
+            results += [flat]
         else: raise Exception(f'No {sel_cal} for this observation')
     elif ch_obs == '23-03-28':
         calibration, lims = collect_fits(ch_obs,'Calibration')
         if sel_cal == 'bias': raise Exception(f'No {sel_cal} for this observation')
         if sel_cal in ['dark', 'all']:
+            # average on different acquisitions
+            mean_dark = Spectrum.empty()
             dark = calibration['dark'] 
-            results += [dark]
+            for d in dark:
+                d = data_file_path(ch_obs,'calibrazione',d)
+                tmp, angle = get_data('dark', d, angle=angle) 
+                mean_dark.hdul += [tmp.hdul]
+                mean_dark.data += [tmp.data]
+            mean_dark.data = np.mean(mean_dark.data,axis=0)
+            # store the result
+            results += [mean_dark]
         if sel_cal in ['flat', 'all']:
+            # average on different acquisitions
+            mean_flat = Spectrum.empty()
             flat = calibration['flat'] 
-            results += [[flat, lims]]
+            for (f, lim) in zip(flat,lims):
+                f = data_file_path(ch_obs,'calibrazione',f)
+                tmp, angle = get_data('flat', f, lims_fit=lim, angle=angle) 
+                mean_flat.hdul += [tmp.hdul]
+                mean_flat.data += [tmp.data]
+                mean_flat.lims = lim
+            mean_flat.data = np.mean(mean_flat.data,axis=0)
+            # store the result
+            results += [mean_flat]
     return results
 
 
