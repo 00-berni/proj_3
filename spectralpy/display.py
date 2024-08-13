ba114c50faf86f660cd358f6c3c250da6ead0f29
00-berni <bernardo.vettori@outlook.it>
2024-08-13 21:14:59 +0200

    Update the function for fits images

--- spectralpy/display.py
+++ spectralpy/display.py
@@ -21,13 +21,14 @@ DISPLAY PACKAGE
 import numpy as np
 from numpy.typing import NDArray
 import matplotlib.pyplot as plt
+from matplotlib.figure import Figure
+from matplotlib.axes import Axes
+from .stuff import Spectrum
+from typing import Sequence, Any
 
 ##*
-def fastplot(x: NDArray, y: NDArray, numfig: int = None, fmt: str = '-', title: str = '', labels: list[str] = ['',''], dim: list[int] = [10,7], grid: bool = False) -> None:
+def fastplot(x: NDArray, y: NDArray, numfig: int = None, fmt: str = '-', title: str = '', labels: list[str] = ['',''], dim: list[int] = [10,7], grid: bool = False,**kwargs) -> None:
     """Function to display a plot quickly.
-    You can choose to make a simple plot or adding some stuff.
-
-    (I wrote it only because of my laziness in writing code).
 
     Parameters
     ----------
@@ -47,44 +48,82 @@ def fastplot(x: NDArray, y: NDArray, numfig: int = None, fmt: str = '-', title: 
         figure size, by default `[10,7]`
     grid : bool, optional
         _description_, by default `False`
+    
+    Notes
+    -----
+    You can choose to make a simple plot or adding some stuff.
+
+    (I wrote it only because of my laziness in writing code).
+
     """
     xl,yl = labels
     plt.figure(numfig,figsize=dim)
     plt.title(title)
-    plt.plot(x,y,fmt)
+    plt.plot(x,y,fmt,**kwargs)
     plt.xlabel(xl)
     plt.ylabel(yl)
     if grid:
         plt.grid(which='both',linestyle='--',alpha=0.2,color='grey')
 
 
-##*
-def showfits(data: NDArray, v: int = -1, title: str = '', subtitle: str = '', n: int = None, dim: list[int] = [10,7]) -> None:
-    """Function to display the fits image.
-    You can display simply the image or set a figure number and a title.
+def fits_image(fig: Figure, ax: Axes, data: Spectrum, v: int = -1, **kwargs) -> None:
+    """To plot fits images
 
     Parameters
     ----------
-    data : NDArray
-        image matrix of fits file
+    fig : Figure
+        figure variable
+    ax : Axes
+        axes variable
+    data : Spectrum
+        target
     v : int, optional
-        cmap parameter, by default `-1`
-        -  `1` for false colors 
-        -  `0` for grayscale 
-        - `-1` for reversed grayscale
-    title : str, optional
-        title of the image, by default `''`
-    n : int, optional
-        figure number, by default `None`
-    dim : list[int], optional
-        figure size, by default `[10,7]`
+        color-code, by default `-1`
+    **kwargs
+        parameters of `matplotlib.pyplot.imshow()`
     """
-    plt.figure(n,figsize=dim)
-    plt.suptitle(title, fontsize=20)
-    plt.title(subtitle, fontsize=18)
+    subtitle = data.name + f', exposure time: {data.get_exposure()} s'
+    ax.set_title(subtitle, fontsize=18)
+    ax.set_xlabel('x [px]')
+    ax.set_ylabel('y [px]')
     if v == 1 : color = 'viridis'
     elif v == 0 : color = 'gray'
     else : color = 'gray_r'
-    plt.imshow(data, cmap=color)
-    plt.colorbar(orientation='horizontal')
+    image = ax.imshow(data.data, cmap=color,**kwargs)
+    cbar = fig.colorbar(image, ax=ax, cmap=color, orientation='horizontal')
+    cbar.set_label('intensity [a.u.]')
+
+##*
+def show_fits(data: Spectrum, num_plots: Sequence[int] = (1,1), dim: Sequence[int] = (10,7), title: str = '', show: bool = False, **kwargs) -> tuple[Figure, Axes | NDArray]:
+    """To plot quickly one or a set of fits pictures
+    
+    Parameters
+    ----------
+    data : Spectrum
+        target
+    num_plots : Sequence[int], optional
+        shape of grid of plots, by default `(1,1)`
+    dim : Sequence[int], optional
+        figure size, by default `(10,7)`
+    title : str, optional
+        title of the image, by default `''`
+    show : bool, optional
+        if `True` it displays the figure, by default `False`
+
+    Returns
+    -------
+    fig : Figure
+        figure
+    axs : Axes | NDArray
+        axes
+    """
+    fig, axs = plt.subplots(*num_plots, figsize=dim) 
+    fig.suptitle(title, fontsize=20)
+    try:
+        for ax in axs:
+            fits_image(fig,ax,data,**kwargs)
+    except TypeError:
+            fits_image(fig,axs,data,**kwargs)
+    if show: plt.show()
+    return fig, axs
 ##*
