c22fa313bece234831f558db4275fc5466888047
00-berni <bernardo.vettori@outlook.it>
2025-02-03 19:11:56 +0100

    Delete method `ccd_response`

--- spectralpy/__init__.py
+++ spectralpy/__init__.py
@@ -1,6 +1,6 @@
 from .data import *
 from .display import *
 from .stuff import Spectrum, FuncFit, print_measure,binning
-from .calcorr import get_target_data, calibration, ccd_response, vega_std
+from .calcorr import get_target_data, calibration, vega_std
 
 TARGETS = open_targets_list()
