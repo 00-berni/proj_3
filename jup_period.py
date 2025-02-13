7a8d5ac8d92d77c8f4e091adb7951eac84703e4e
00-berni <bernardo.vettori@outlook.it>
2025-02-13 01:12:30 +0100

    Correct the acquisition mistake

--- jup_period.py
+++ jup_period.py
@@ -187,17 +187,31 @@ plt.axhline(top,0,1,color='red')
 plt.axhline(low,0,1,color='red')
 plt.show()
 
+lamp = lamp.rotate_target(angle=angle)
 
 ### Wavelength Calibration
 # average over 4 rows
-heights = np.array([710+i*1 for i in range(4)])
+heights = np.array([750+i*1 for i in range(10)])
+plt.figure()
+plt.imshow(lamp.data,aspect='auto',origin='lower')
+plt.axhline(700,0,1)
+plt.axhline(800,0,1)
+
+incl = 10
+incl_ang = np.arctan(incl)*180/np.pi -90
+print(incl_ang)
+lamp = lamp.rotate_target(angle=incl_ang)
+plt.figure()
+plt.imshow(lamp.data,aspect='auto',origin='lower')
+plt.show()
+
 lamp.spec, lamp.std = spc.mean_n_std(lamp.data[heights],axis=0)
 # get data
 lines, px, Dpx = spc.get_cal_lines(OBS_NIGHT,TARGET)
 Dlines = lines/20000 / 2
 plt.figure()
 plt.title('Lamp spectrum',fontsize=FONTSIZE)
-plt.errorbar(np.arange(*lamp.spec.shape),lamp.spec,lamp.std)
+plt.errorbar(np.arange(*lamp.spec.shape),lamp.spec,lamp.std,fmt='.-')
 for pxi,Dpxi in zip(px,Dpx):
     plt.axvline(pxi,0,1,color='red',linestyle='dashdot')
     plt.axvspan(pxi-Dpxi,pxi+Dpxi,facecolor='orange',alpha=0.4)
@@ -209,6 +223,14 @@ plt.show()
 fit = spc.FuncFit(xdata=px,xerr=Dpx,ydata=lines,yerr=Dlines)
 fit.linear_fit(mode='curve_fit')
 fit.plot(mode='subplots',points_num=3,plotargs={'title':'Wavelength calibration','ylabel':'$\\lambda$ [$\\AA$]','fontsize': FONTSIZE},xlabel='x [px]',fontsize=FONTSIZE)
+plt.show()
+
+plt.figure()
+plt.imshow(jupiter.data)
+plt.xticks(np.arange(jupiter.data.shape[1])[::30],fit.method(np.arange(jupiter.data.shape[1])[::30]+jupiter.lims[2]))
+
+plt.figure()
+plt.plot(fit.method(np.arange(jupiter.data.shape[1])+jupiter.lims[2]),jupiter.data.sum(axis=0))
 plt.show()
 
 ### Period Estimation
@@ -239,7 +261,7 @@ for sel_cut in cut_list:
     plt.axvline(sel_cut[1],0,1,color='w',linestyle='dashdot')
 plt.show()
 (mf, qf), (Dmf, Dqf) = fit.results()
-
+pcov = fit.res['cov'].copy()
 
 sel_cut = (676,705)
 p1 = data[px_u,slice(*sel_cut)]
@@ -325,6 +347,7 @@ DT3 = (8*np.pi*R/C).to(u.h) * ((Dminpos + np.sqrt((Dqf/mf)**2 + (qf*Dmf/mf**2)**
 # DT3 = (8*np.pi*R/C/corr_shift).to(u.h) * (Dminpos + np.sqrt((Dqf/mf)**2 + (qf*Dmf/mf**2)**2 + 2*fit.res['cov'][0,1]*qf/mf**3)) 
 # DT3 = (T3-(8*np.pi*R/C*minpos/shift3).to(u.h)) * np.sqrt((Dmf/mf)**2+(Dqf/qf)**2+2*fit.res['cov'][0,1]/mf/qf)
 # DT3 = (T3-8*np.pi*R/C*minpos/shift3).to(u.h) * np.sqrt((Dmf/mf)**2+(Dqf/qf)**2)
+print('CONST',(8*np.pi*R/C).to(u.h))
 print((C*(corr_shift*mf)/minval/4).to(u.km/u.s))
 print(T3,DT3,DT3/T3*100,'%')
 print(PERIOD)
@@ -353,3 +376,104 @@ DT3 = (8*np.pi*R/C/corr_shift).to(u.h) * (Dminpos + Dha_px + BALMER*Dmf/mf**2 + 
 print((8*np.pi*R/C/corr_shift).to(u.h) *( Dha_px + BALMER*Dmf/mf**2))
 print((8*np.pi*R/C/corr_shift).to(u.h) *(  (minpx+ha_px + BALMER/mf)*Dcorr_shift/corr_shift))
 print('New T3',T3,DT3)
+print('\n\n========\n')
+plt.figure()
+plt.imshow(jupiter.rotate_target(angle=incl_ang).data,vmin=data.min(),vmax=data.max(),origin='lower',aspect='auto')
+for i in [271,361,608,717,960]:
+    plt.axvline(i,0,1,linestyle='dashed')
+# plt.plot([px_ul[0],px_ur[0]],[px_ul[1],px_ur[1]])
+# plt.plot([px_dl[0],px_dr[0]],[px_dl[1],px_dr[1]])
+corr_jup = jupiter.rotate_target(angle=incl_ang)
+corr_jup.lims = np.array([561,798,0,1363])
+corr_jup.cut_image()
+data = corr_jup.data.copy()
+plt.figure()
+plt.imshow(data,origin='lower',aspect='auto',cmap='gray')
+plt.figure()
+plt.plot(fit.method(np.arange(data.shape[1])+corr_jup.lims[2]),data.sum(axis=0))
+pos_halpha = data.sum(axis=0).argmin()+corr_jup.lims[2]
+new_halpha = fit.method(pos_halpha)
+Dnew_halpha = abs(mf+np.sqrt((Dmf*pos_halpha)**2+Dqf**2+2*pcov[0,1]*pos_halpha))
+v_bulk = C.to(u.km/u.s)/2*(new_halpha-BALMER)/BALMER
+Dv_bulk = abs(C.to(u.km/u.s)/2*Dnew_halpha/BALMER)
+print('New Halpha',new_halpha,'+-',Dnew_halpha,Dnew_halpha/new_halpha*100,BALMER,new_halpha-BALMER)
+print('V bulk',v_bulk,'+-',Dv_bulk)
+
+def new_computation(sel_line: tuple[int,int], sel_range: tuple[tuple[int,int],tuple[int,int]]):
+    htp, hbt = sel_line      #: heights
+    rtp, rbt = sel_range     #: widths
+    jtp = data[htp,slice(*rtp)]
+    jbt = data[hbt,slice(*rbt)]
+    jsum = data[slice(hbt,htp),slice(rtp[0],rbt[-1])].sum(axis=0)
+    px_diff = abs((jtp.argmin()+rtp[0])-(jbt.argmin()+rbt[0]))
+    diff = px_diff*mf
+    
+    midpos  = jsum.argmin() if jsum.argmin() != 1223 else ((jtp.argmin()+rtp[0])+(jbt.argmin()+rbt[0]))/2 - rtp[0]
+    # T = CONST * (x + q/m)/(pxdiff)
+    # DT = CONST * (1/pxdiff + midline/diff * 1/pxdiff + sqrt((q*Dm/m**2)**2 + (Dq/m)**2 + 2pcov[0,1]*q/m**3)/pxdiff)
+    midline = fit.method(midpos+rtp[0]+corr_jup.lims[2])
+    period = (np.pi*8*R/C).to(u.h)*midline/diff 
+    error  = (np.pi*8*R/C).to(u.h) * (1/px_diff + midline/diff * 1/px_diff + np.sqrt((qf*Dmf/mf**2)**2 + (Dqf/mf)**2 + 2*pcov[0,1]*qf/mf**3)/px_diff)
+    print('\nResults:',sel_line,midpos+rtp[0]+corr_jup.lims[2])
+    print((jtp.argmin()+rtp[0]),(jbt.argmin()+rbt[0]))
+    print('PERIOD',period,'+-',error,'diff',period-PERIOD,(period-PERIOD)/error)
+    # T = CONST * (m*(x-xalpha) + Halpha)/(m*pxdiff)
+    # DT = CONST * ((Dx+Dxalpha)/pxdiff + (m*(x-xalpha) + Halpha)/(m*pxdiff)*Dpxdiff/pxdiff)
+    corr_p = (np.pi*8*R/C).to(u.h)*(midpos+rtp[0]+corr_jup.lims[2]-pos_halpha+BALMER/mf)/px_diff
+    corr_e = (np.pi*8*R/C).to(u.h)/px_diff*(2+(midpos+rtp[0]+corr_jup.lims[2]-pos_halpha+BALMER/mf)/px_diff+BALMER*Dmf/mf**2)
+    print('CORR PERIOD',corr_p,'+-',corr_e,'diff',corr_p-PERIOD,(corr_p-PERIOD)/corr_e)
+    plt.figure()
+    plt.imshow(data[slice(hbt-4,htp+4),slice(rtp[0],rbt[-1])],cmap='gray',origin='lower')
+    plt.axhline(4,0,1,linestyle='dashed',color='blue',label='bottom')
+    plt.axhline((htp-hbt)+4,0,1,linestyle='dashed',color='orange',label='top')
+    plt.legend()
+    plt.figure()
+    plt.plot(jtp,color='orange',label='top')
+    plt.plot(jbt,color='blue',label='bottom')
+    plt.legend()
+    plt.grid()
+    plt.figure()
+    plt.plot(jsum)
+    plt.errorbar(midpos,jsum.min(),xerr=1,color='red',capsize=3)
+    plt.grid()
+    plt.show()
+    return period.to(u.h).value
+
+selected_lines = [
+                    [(154,64),((622,634),(635,647))      ],
+                    [(186,96),((908,920),(921,935))      ],
+                    [(218,123),((1205,1219),(1219,1233)) ],
+                    [(218,123),((1219,1233),(1233,1250)) ],
+                    [(220,131),((1264,1280),(1278,1292)) ]
+                ]
+
+fig,ax = plt.subplots(1,1)
+# plt.imshow(data,cmap='gray',origin='lower')
+spc.fits_image(fig,ax,corr_jup,v=0,subtitle=None,vmax=7000,origin='lower',aspect='equal')
+ax.set_title('Selected Lines',fontsize=FONTSIZE+5)
+for sel in selected_lines:
+    top, bottom = sel[0]
+    lends, rends = sel[1]
+    if lends[0] != 1219:
+        ax.plot([lends[0],lends[0]],[bottom-4,top+4],linestyle='dashed',color='white')
+    if rends[0] != 1219:
+        ax.plot([rends[-1],rends[-1]],[bottom-4,top+4],linestyle='dashed',color='white')
+plt.show()
+periods = [ new_computation(*sel) for sel in selected_lines ]
+
+mean_period, std_period = spc.mean_n_std(periods)
+print('\nResult')
+spc.print_measure(mean_period,std_period,'P','h')
+print(mean_period-PERIOD.value,(mean_period-PERIOD.value)/std_period)
+
+# [186,908:920]
+# [96,921:935]
+
+# [218,1205:1219]
+# [123,1219:1233]
+
+# [218,1219:1232]
+# [123,1233:1250]
+
+# [220,1264:1280]
+# [131,1278:1292]
